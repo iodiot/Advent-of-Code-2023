cd2b338a078fdfcd92bf33334126a75f0febc11f
Mike <iodiot@gmail.com>
2023-12-08 13:19:32 +0200

    refactor: `Day 8: Haunted Wasteland`

--- 8/solution.js
+++ 8/solution.js
@@ -4,20 +4,17 @@ import fs from 'fs';
 
 console.time();
 
-function countSteps(grid, instructions, nodes) {
-  for (let i = 0; ; ++i) {
+function countSteps(grid, instructions, node) {
+  for (let i = 0;; ++i) {
     const instruction = instructions[i % instructions.length];
-    nodes = nodes.map(node => grid[node][instruction]);
-
-    if (nodes.every(node => node[2] === 'Z')) {
-      return i + 1;
-    }
+    node = grid[node][instruction];
+    if (node[2] === 'Z') return i + 1;
   }
 }
 
 const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
 const lcm = (a, b) => (a * b) / gcd(a, b);
-const lcmArray = (arr) => (arr.length === 0 ? null : arr.reduce((result, num) => lcm(result, num), arr[0]));
+const lcmArray = (arr) => arr.reduce((result, num) => lcm(result, num), arr[0]);
 
 const input = fs.readFileSync('./input.txt', 'utf8').trim();
 const lines = input.split('\r\n').filter(line => line.length > 0);
@@ -29,9 +26,9 @@ lines.map(line => line.match(/[0-9A-Z]{3}/g)).forEach(([a, b, c]) => {
 });
 
 const nodes = Object.keys(grid).filter(key => key[2] === 'A');
-const steps = nodes.map(node => countSteps(grid, instructions, [node]));
+const steps = nodes.map(node => countSteps(grid, instructions, node));
 
-console.log(`part 1: ${ countSteps(grid, instructions, ['AAA']) }`);
+console.log(`part 1: ${ countSteps(grid, instructions, 'AAA') }`);
 console.log(`part 2: ${ lcmArray(steps) }`);
 
 console.timeEnd();
