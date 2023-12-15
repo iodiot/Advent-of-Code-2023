9d6de8e112f094ef3ec4f0312971093239acf786
Mike <iodiot@gmail.com>
2023-12-15 13:07:13 +0200

    feat: Stubs for other days

---
+++ 16/solution.js
@@ -0,0 +1,12 @@
+//
+
+import fs from 'fs';
+
+console.time();
+
+const input = fs.readFileSync('./input.txt', 'utf8').trim();
+
+console.log(`part 1: ${ 0 }`);
+console.log(`part 2: ${ 0 }`);
+
+console.timeEnd();
