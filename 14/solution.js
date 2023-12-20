4cbb63f3125526b6bb037c1f2d3b375a7312aa5a
Mike <iodiot@gmail.com>
2023-12-20 11:47:07 +0200

    feat: `Day 14: Parabolic Reflector Dish` (first part)

--- 14/solution.js
+++ 14/solution.js
@@ -1,12 +1,44 @@
-//
+// --- Day 14: Parabolic Reflector Dish ---
 
 import fs from 'fs';
 
 console.time();
 
-const input = fs.readFileSync('./input.txt', 'utf8').trim();
+function tilt(platform) {
+  const get = (x, y) => platform[y] && platform[y][x] || ' ';
+  const set = (x, y, v) => platform[y][x] = v;
 
-console.log(`part 1: ${ 0 }`);
+  for (let y = 0; y < platform.length; y++) {
+    for (let x = 0; x < platform[y].length; x++) {
+      if (get(x, y) !== 'O') continue;
+
+      let yy = y - 1;
+
+      while (get(x, yy) === '.') {
+        set(x, yy, 'O');
+        set(x, yy + 1, '.');
+        --yy;
+      }
+    }
+  }
+
+  return platform;
+}
+
+function countLoad(platform) {
+  let load = 0;
+
+  for (let y = 0; y < platform.length; y++) {
+    load += platform[y].filter(v => v === 'O').length * (platform.length - y);
+  }
+
+  return load;
+}
+
+const platform = fs.readFileSync('./input.txt', 'utf8').trim().split('\r\n')
+  .map(row => row.split(''));
+
+console.log(`part 1: ${ countLoad(tilt(platform)) }`);
 console.log(`part 2: ${ 0 }`);
 
 console.timeEnd();
